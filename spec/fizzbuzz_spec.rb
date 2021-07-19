c8f031b1160f07c064a5940ea25adda2651eee8b
Te Hotu <hotunz@gmail.com>
2021-07-19 16:06:55 +0100

    add tests and method

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require 'fizzbuzz'
+
+  describe 'fizzbuzz' do
+    it 'returns "fizz" for multiples of 3, "buzz" for multiples of 5 and "fizzbuzz" for multiples of both' do
+      (1..15).each do |number|
+        if (number % 3 == 0) && (number % 5 == 0)
+          expect(fizzbuzz(number)).to eq "fizzbuzz"
+        elsif (number % 3 == 0)
+          expect(fizzbuzz(number)).to eq "fizz"
+        elsif (number % 5 == 0)
+          expect(fizzbuzz(number)).to eq "buzz"
+        else 
+          number
+        end
+    end
+  end
